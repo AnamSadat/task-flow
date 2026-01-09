1f5b9a6f67ddeee11a0df05ae9183a8eb01e2ec4
AnamSadat <anamsadat3@gmail.com>
2026-01-09 19:28:16 +0700

    feat(utils): add helper generated_id

---
+++ internal/utils/generate_id.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+func GenerateID() (string, error) {
+	b := make([]byte, 16)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	return base64.RawURLEncoding.EncodeToString(b), nil
+}
