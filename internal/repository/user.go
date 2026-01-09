71a9dd9faaec49834a9feac1059bacffff042792
AnamSadat <anamsadat3@gmail.com>
2026-01-09 20:15:28 +0700

    feat(repo): update repo user

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -7,6 +7,7 @@ import (
 )
 
 type UserRepo interface {
+	Create(ctx context.Context, user model.User) error
 	FindByID(ctx context.Context, id string) (model.User, bool, error)
 	FindByEmail(ctx context.Context, email string) (model.User, bool, error)
 }
