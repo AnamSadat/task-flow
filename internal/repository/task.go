8899f41fed1f5cd23e4b4d3c8ce23a7fc4a94ee8
AnamSadat <anamsadat3@gmail.com>
2026-01-10 22:29:22 +0700

    feat(repo): add findbyid and deletetask in repo

--- internal/repository/task.go
+++ internal/repository/task.go
@@ -9,4 +9,6 @@ import (
 type TaskRepo interface {
 	AddTask(ctx context.Context, task model.Task) error
 	GetTasks(ctx context.Context) ([]model.Task, error)
+	FindByID(ctx context.Context, id string) (model.Task, error)
+	DeleteTask(ctx context.Context, id string) error
 }
